1bbbad8a011c089ca7e4802dcd47df61d9adfc07
wallmartins <wall_martins_@hotmail.com>
2021-05-28 16:45:40 -0300

    feat(update): update RegisterStorage add request to get all states and implement all state options in register form

--- src/Context/RegisterStorage.jsx
+++ src/Context/RegisterStorage.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { listAllStates } from '../services/firebase/States';
 
 export const RegisterContext = React.createContext();
 
-export const RegisterStorage = ({ children }) => (
-  <RegisterContext.Provider value={{}}>{children}</RegisterContext.Provider>
-);
+export const RegisterStorage = ({ children }) => {
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [states, setStates] = React.useState([]);
+  React.useEffect(() => {
+    try {
+      setError(null);
 
-export const withStreamerContext = (WrappedComponent) => (props) =>
+      const getAllState = async () => {
+        const response = await listAllStates();
+        const statesList = response.map((state) => state.name);
+        const allStates = statesList.sort();
+        setStates(allStates);
+      };
+      getAllState();
+      setLoading(false);
+    } catch (err) {
+      setError(`Não foi possível possível pegar os estados ${err}`);
+    } finally {
+      setError(null);
+      setLoading(false);
+    }
+  }, []);
+
+  return <RegisterContext.Provider value={{ states, loading, error }}>{children}</RegisterContext.Provider>;
+};
+
+export const withRegisterContext = (WrappedComponent) => (props) =>
   (
     <RegisterStorage>
       <WrappedComponent {...props} />
