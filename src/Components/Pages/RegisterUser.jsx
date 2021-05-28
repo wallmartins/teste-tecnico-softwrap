d1d4341fe68dff52770bd2492169140333f6d9de
wallmartins <wall_martins_@hotmail.com>
2021-05-28 15:46:16 -0300

    feat(add): add register form in RegisterUser page

--- src/Components/Pages/RegisterUser.jsx
+++ src/Components/Pages/RegisterUser.jsx
@@ -1,3 +1,113 @@
-const RegisterUser = () => <div>Register</div>;
+const RegisterUser = () => (
+  <div className="container mx-auto my-52">
+    <div className="md:grid md:grid-cols-3 md:gap-6">
+      <div className="px-8 md:col-span-1">
+        <div className="px-4 sm:px-0">
+          <h3 className="text-lg font-bold leading-6 text-gray-900">Informações Pessoais</h3>
+          <p className="mt-1 text-sm text-gray-500">Adicione aqui as informações da nova pessoa.</p>
+        </div>
+      </div>
+      <div className="mt-5 md:mt-0 md:col-span-2">
+        <form action="#" method="POST">
+          <div className="shadow overflow-hidden sm:rounded-md">
+            <div className="px-4 py-5 bg-white sm:p-6">
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+                    Nome
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    autoComplete="given-name"
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
 
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="age" className="block text-sm font-medium text-gray-700">
+                    Idade
+                  </label>
+                  <input
+                    type="text"
+                    name="age"
+                    id="age"
+                    autoComplete="age"
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="maritalStatus" className="block text-sm font-medium text-gray-700">
+                    Estado Civil
+                  </label>
+                  <select
+                    id="maritalStatus"
+                    name="maritalStatus"
+                    autoComplete="maritalStatus"
+                    className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  >
+                    <option>Solteiro(a)</option>
+                    <option>Casado(a)</option>
+                    <option>Separado(a)</option>
+                    <option>Divorciado(a)</option>
+                    <option>Viuvo(a)</option>
+                  </select>
+                </div>
+
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="email_address" className="block text-sm font-medium text-gray-700">
+                    Email address
+                  </label>
+                  <input
+                    type="text"
+                    name="email_address"
+                    id="email_address"
+                    autoComplete="email"
+                    className="mt-1 w-full focus:ring-indigo-500 focus:border-indigo-500 shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3 lg:col-span-3">
+                  <label htmlFor="city" className="block text-sm font-medium text-gray-700">
+                    Cidade
+                  </label>
+                  <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                  />
+                </div>
+
+                <div className="col-span-6 sm:col-span-3 lg:col-span-3">
+                  <label htmlFor="state" className="block text-sm font-medium text-gray-700">
+                    Estado
+                  </label>
+                  <select
+                    id="state"
+                    name="state"
+                    autoComplete="state"
+                    className="mt-1 w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  >
+                    <option>Rio de Janeiro</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+              <button
+                type="submit"
+                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Cadastrar
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+);
 export default RegisterUser;
